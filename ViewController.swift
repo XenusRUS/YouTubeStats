84264e53c697f217418791f5aa31e8f600e1038e
XenusRUS <xenus.doc@yandex.ru>
2016-10-25 21:01:54 +0500

    API connect

---
+++ ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Youtube Stats
+//
+//  Created by admin on 11.10.16.
+//  Copyright © 2016 vadim. All rights reserved.
+// AIzaSyDLvbN2Z-s_MElyKBFZ4R06K12PvGtyZy4 API KEY
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var channelNameTextField: UITextField!
+    
+    let APIkey = "AIzaSyDLvbN2Z-s_MElyKBFZ4R06K12PvGtyZy4"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        getStat()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getStat() {
+        Alamofire.request(.GET, "https://www.googleapis.com/youtube/v3/channels", parameters: ["part":"snippet", "forUsername":channelNameTextField.text!, "key":APIkey], encoding: ParameterEncoding.URL, headers: nil).responseJSON { (response) -> Void in
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+
+        }
+    }
+    
+    @IBAction func confirmButton(sender: AnyObject) {
+        if channelNameTextField.text == "" {
+            let alertController = UIAlertController(title: "Поле ввода ключа не может быть пустым!", message:"Введите код активации терминала", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        else {
+            getStat()
+        }
+    }
+    
+
+
+}
+
